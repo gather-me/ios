f458ccce6d8d06dbb8e39ff9f2e6241c3850b73b
Özgür Deniz Türker <ozgur.deniz.turker@valensas.com>
2023-05-13 09:21:25 +0300

    Initial Commit

---
+++ gather/gatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  gatherApp.swift
+//  gather
+//
+//  Created by Ozgur on 13.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct gatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
